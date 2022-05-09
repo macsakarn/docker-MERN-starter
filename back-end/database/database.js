319161dd8f505e3a58f0fd955e1d9acc74e6f950
Sakarn <59902184+macsakarn@users.noreply.github.com>
2022-05-10 04:39:08 +0700

    Update database.js

--- back-end/database/database.js
+++ back-end/database/database.js
@@ -1,5 +1,5 @@
 const configs = {
-    mongouri : "mongodb+srv://macsakarn:macsakarn@accounts.1v2g1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongouri : "<URL โว้ยยยยยย>"
 }
 
 module.exports = configs
